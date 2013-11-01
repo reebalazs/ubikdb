f27840ca73d430851a715dd46fa3e16e97f2b3c6
Balazs Ree <ree@greenfinity.hu>
2013-11-01 22:16:51 +0100

    Start writing an UbikDB wrapper class.

--- ubikdb/static/ubikdb.js
+++ ubikdb/static/ubikdb.js
@@ -9,5 +9,32 @@
     //window.WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
     window.WEB_SOCKET_DEBUG = true;
 
+    function UbikDB(url) {
+        this.url = url;
+    }
+
+    UbikDB.prototype.init = function() {
+        // connect to the websocket
+        var socket = this.socket = io.connect('/ubikdb');
+        window.beforeunload = function() {
+            socket.disconnect();
+        };
+        this.eventMap = {};
+    };
+
+    UbikDB.prototype.trigger = function(type) {
+        var method = this['on_' + type];
+        return method.apply(this, Array.prototype.slice(arguments, 1));
+    };
+
+    UbikDB.prototype.on = function(type) {
+
+    };
+
+    window.UbikDB = function() {
+        var ubikDB = new UbikDB(Array.prototype.splice(arguments));
+        ubikDB.init();
+        return ubikDB;
+    };
 
 }();
